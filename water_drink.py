584f1dd04b06dabfed6e47f8f595464335202dbf
Darshilshah97 <darshilrshah1997@gmail.com>
2023-07-01 22:12:18 -0400

    Second draft for water drinking reminder app

--- water_drink.py
+++ water_drink.py
@@ -3,18 +3,21 @@ from plyer import notification
 from datetime import datetime
 
 count = 1
-time_interval = int(input('Please enter the time interval(in seconds)  you want to receive water drinking notification.'))
+name = input("Enter your name: ")
+minutes = int(input(f'{name}, after how many each minutes you want to receive water drinking reminder (Please enter minutes in whole numbers): '))
+
+
 print('Info: It send 2 notifications for the interval you entered.')
 
-while(count < 3):
+while(True):
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     notification.notify(
         title = 'Water Drinking Reminder ' + str(count) + ' at time ' + current_time,
-        message = 'Hey Darshil, Don\'t forget to drink water',
+        message = f'Hey {name}, Don\'t forget to drink water',
         app_icon = 'Waterglass.ico',
         timeout = 10,
     )
     count = count + 1
-    time.sleep(time_interval)
+    time.sleep(minutes*60)
